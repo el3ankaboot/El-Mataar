49f58e372a3a23ecdcff15da647aae0114833418
el3ankaboot <gamal.m.esmail@gmail.com>
2021-10-02 02:03:09 +0200

    Implement the view of adding new airlines

--- El-Mataar/Helpers/Extensions/UIViewsExtensions.swift
+++ El-Mataar/Helpers/Extensions/UIViewsExtensions.swift
@@ -8,13 +8,15 @@
 import Foundation
 import UIKit
 
+//MARK:- UIView
 extension UIView {
+    //MARK: Corner Radius
     @IBInspectable
     var viewCornerRadius: CGFloat{
         set{self.layer.cornerRadius = newValue;self.clipsToBounds = true}
         get{return self.viewCornerRadius}
     }
-
+    //MARK: Shadow
     func showShadow(){
         self.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
         self.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
@@ -22,9 +24,23 @@ extension UIView {
         self.layer.shadowOpacity = 0.5
         self.layer.masksToBounds = false
     }
+    
+    //MARK: Border
+    @IBInspectable
+    var borderColor: UIColor{
+        set{self.layer.borderColor = newValue.cgColor}
+        get{return self.borderColor}
+    }
+    @IBInspectable
+    var borderWidth: CGFloat{
+        set{self.layer.borderWidth = newValue}
+        get{return self.layer.borderWidth}
+    }
 }
 
+//MARK:- UITableView
 extension UITableView {
+    //MARK: Dequeue Reusable Cell
     public func dequeueReusableCell<T: UITableViewCell>(with type: T.Type, for indexPath: IndexPath) -> T {
         return self.dequeueReusableCell(withIdentifier: String(describing: type.self), for: indexPath) as! T
     }
